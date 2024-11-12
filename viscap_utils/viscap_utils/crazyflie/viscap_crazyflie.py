846676a43b835aa507acef6fa3327499bb8efa36
Eduard0Castro <americogomes1@outlook.com>
2024-11-11 22:54:49 -0300

    Initial commit: viscap_utils ros2 package, Crazyflie class and examples

---
+++ viscap_utils/viscap_utils/crazyflie/viscap_crazyflie.py
@@ -0,0 +1,97 @@
+from rclpy.node import Node
+import cflib.crtp as crtp
+from cflib.crazyflie import Crazyflie
+from cflib.crazyflie.syncCrazyflie import SyncCrazyflie
+from cflib.utils import uri_helper
+from cflib.utils.multiranger import Multiranger
+from cflib.positioning.motion_commander import MotionCommander
+from cflib.positioning.position_hl_commander import PositionHlCommander
+
+
+class ViscapCrazyflie:
+
+    URI = uri_helper.uri_from_env(default='radio://0/80/2M/E7E7E7E7E7')
+    MULTIRANGER = "multiranger"
+
+    def __init__(self, node: Node) -> None:
+
+        """
+        Initialize the crazyflie's class objects for Crazyflie, SyncCrazyflie, MotionCommander and 
+        Multiranger
+        :param node (rclpy.node.Node): the ROS2 node to handle the crazyflie actions
+        """
+
+        self.node = node
+
+        self.crazyflie = Crazyflie(rw_cache="./cache")
+        self.sync_crazyflie = SyncCrazyflie(link_uri = ViscapCrazyflie.URI, cf = self.crazyflie)
+
+        self.motion = MotionCommander(self.sync_crazyflie, 0.5)
+        # self.position_hl = PositionHlCommander(self.sync_crazyflie)
+
+        self.cleaned = False
+
+        self.tools = {ViscapCrazyflie.MULTIRANGER: lambda : Multiranger(self.sync_crazyflie)}
+        self.__initialized_tools = list()
+
+    def init_crazyflie(self, tools: list, height: float = 0.5) -> None:
+
+        """
+        Initialize the drivers, crazyflie's communication settings and make it take off
+
+        :param tools (list): list with the tools to initialize 
+        :param height (float): the height in meters for drone takeoff 
+        """
+
+        crtp.init_drivers()
+        self.sync_crazyflie.open_link()
+        self.motion.take_off(height=height)
+
+        for tool in tools:
+            action = self.tools.get(tool, None)
+            if action:
+                tool_ = action()
+                tool_.__enter__()
+                self.__initialized_tools.append(tool_)
+
+            else: raise ValueError("Invalid tool name")
+
+        return self.__initialized_tools[0]
+
+    def multiranger_navigate(self) -> None:
+        """
+        Monitore the 5 sensors in the multiranger deck and avoid colision
+        """
+        self.node.get_logger().info("Multiranger monitor timer initialized")
+        self.multiranger_timer = self.node.create_timer(0.1, self.__multiranger_monitor)
+        self.__vel = 0.5
+
+    def __multiranger_monitor(self) -> None:
+        velocity_x = 0
+        velocity_y = 0
+
+        if self.__is_close(self.multiranger.front): velocity_x -= self.__vel
+        if self.__is_close(self.multiranger.back) : velocity_x += self.__vel
+        if self.__is_close(self.multiranger.right): velocity_y += self.__vel
+        if self.__is_close(self.multiranger.left) : velocity_y -= self.__vel
+        if self.__is_close(self.multiranger.up)   : 
+            self.motion.land()
+            self.node.destroy_timer(self.multiranger_timer)
+
+        self.motion.start_linear_motion(velocity_x, velocity_y, 0)
+
+    def __is_close(self, range: float) -> bool:
+        if range is None: return False
+        else: return range < 0.2
+
+    def cleanup(self) -> None:
+
+        if not self.cleaned: 
+            self.motion.land()
+            [tool.__exit__(None, None, None) for tool in self.__initialized_tools]
+            self.sync_crazyflie.close_link()
+            self.cleaned = True
+        else: print("Already closed")
+
+    
+        
---
+++ viscap_utils/viscap_utils/examples/multiranger_test.py
@@ -0,0 +1,57 @@
+import rclpy
+from rclpy.node import Node
+from time import sleep
+from viscap_utils.crazyflie.viscap_crazyflie import ViscapCrazyflie
+
+class MultirangerTest(Node):
+
+    def __init__(self) -> None:
+        
+        super().__init__("test_multiranger_node")
+
+        self.crazyflie = ViscapCrazyflie(self)
+        self.multiranger = self.crazyflie.init_crazyflie([ViscapCrazyflie.MULTIRANGER])
+
+        self.get_logger().info("Test Multiranger Node has been initialized")
+
+    def teste_gate(self) -> None:
+
+        self.crazyflie.motion.up(1.3)
+        self.crazyflie.motion.forward(1.0)
+        sleep(0.5)
+        self.crazyflie.motion.down(0.6)
+        
+        while self.multiranger.front <= 0.15:
+            self.crazyflie.motion.start_linear_motion(0, 0.5, 0)
+
+        self.crazyflie.motion.forward(0.5)
+        self.crazyflie.motion.land()
+        
+
+    def run_test(self) -> None:
+        self.crazyflie.multiranger_navigate()
+
+def main(args = None):
+    
+    rclpy.init(args=args)
+
+    try:
+        test = MultirangerTest()
+        test.run_test()
+        # test.teste_gate()
+        rclpy.spin(test)
+
+    except KeyboardInterrupt as ki:
+        print(ki)
+
+    except Exception:
+        pass
+
+    finally:
+        test.crazyflie.cleanup()
+        test.destroy_node()
+
+if __name__ == "__main__":
+    main()
+
+
